137eefe116b6dcd67a3861cd0f22ac49a0a7f94d
Anastasia <great-swindler@mail.ru>
2019-08-23 19:55:54 +0300

    Initial commit

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      location: '',
+      sortBy: 'best_match'
+    };
+    this.sortByOptions = {
+      'Best Match': 'best_match',
+      'Highest Rated': 'rating',
+      'Most Reviewed': 'review_count',
+    };
+  }
+
+    // getSortByClass returns the current CSS class for a sorting option
+    getSortByClass(sortByOption) {
+      if (this.state.sortBy === sortByOption) {
+        return 'active';
+      } else {
+        return ''
+      }
+    }
+
+    handleSortByChange(sortByOption)  {
+      this.setState({
+        sortBy: sortByOption
+      });
+    };
+
+    renderSortByOptions() {
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return <li 
+            className={this.getSortByClass(sortByOptionValue)} 
+            key={sortByOptionValue}
+            onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+            >
+            {sortByOption}
+            </li>;
+            });    
+    };
+    
+    // Last completed task : 9 (handleSort to stuff)
+
+    render() {
+        return (
+            <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>
+            {this.renderSortByOptions()}
+           </ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          <a>Let's Go</a>
+        </div>
+      </div>
+        )
+    }
+};
+
+export default SearchBar;
